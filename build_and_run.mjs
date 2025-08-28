aa563d1278508d53313c66855a86814b94bdc370
Guthib-of-Dan <diril656@gmail.com>
2025-08-28 16:02:21 +0000

    New commit

--- build_and_run.mjs
+++ build_and_run.mjs
@@ -1,14 +1,13 @@
 import {arch, platform} from "node:os";
 import {exec} from "node:child_process";
 import {log} from "node:console";
-var output = platform() + '-' + arch() + ".exe";
 var compiler = platform() == "darwin" ? "clang++" : "g++";
-exec(compiler + " -s -O3 -o " + output + " main.cpp", (err, stdout, stderr)=>{
+exec(compiler + " -s -O3 -o " + "./binary.exe" + " main.cpp", (err, stdout, stderr)=>{
   log("ERR", err);
   log("stdout: ", stdout);
   log("stderr: ", stderr);
   log("START BUILD");
-  exec("./" + output, (err, stdout, stderr)=>{
+  exec("./binary.exe", (err, stdout, stderr)=>{
     log("ERR", err);
     log("stdout: ", stdout);
     log("stderr: ", stderr);
